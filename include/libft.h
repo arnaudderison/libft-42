09d7679b7a0010fdf1f19e2881e911660b9f09ea
arnaudderison <arnaud.derison@hotmail.com>
2023-09-01 09:45:33 +0200

    libft.h

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: arnaud <arnaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 22:41:28 by arnaud            #+#    #+#             */
-/*   Updated: 2023/08/31 08:43:01 by arnaud           ###   ########.fr       */
+/*   Updated: 2023/09/01 09:44:49 by arnaud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,13 @@
 # include <unistd.h>
 
 void	ft_bzero(void *s, size_t n);
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
-int		ft_strlen(const char *s);
 char	*ft_strdup(char *str);
+int		ft_strlen(const char *s);
 
 #endif
