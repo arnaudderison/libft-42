842a304312c86cc97016942f245b547a808390d5
arnaudderison <arnaud.derison@hotmail.com>
2023-09-01 09:55:42 +0200

    ft_memchr.c

--- src/ft_memcmp.c
+++ src/ft_memcmp.c
@@ -6,12 +6,25 @@
 /*   By: arnaud <arnaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 09:36:30 by arnaud            #+#    #+#             */
-/*   Updated: 2023/09/01 09:48:04 by arnaud           ###   ########.fr       */
+/*   Updated: 2023/09/01 09:54:22 by arnaud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memchr(const void *s, const void *s2, size_t n)
+void	*ft_memchr(const void *s1, const void *s2, size_t n)
 {
+	const unsigned char	*s1_ptr;
+	const unsigned char	*s2_ptr;
+
+	s1_ptr = (const unsigned char *)s1;
+	s2_ptr = (const unsigned char *)s2;
+	while (n--)
+	{
+		if (*s1_ptr != *s2_ptr)
+			return (*s1_ptr - *s2_ptr);
+		s1_ptr++;
+		s2_ptr++;
+	}
+	return (0);
 }
