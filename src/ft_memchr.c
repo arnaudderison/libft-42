7448539c4939074f59d00325e5f307b8213d2417
arnaudderison <arnaud.derison@hotmail.com>
2023-09-01 09:40:33 +0200

    fix laste bug on ft_memchr

--- src/ft_memchr.c
+++ src/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: arnaud <arnaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 21:23:28 by arnaud            #+#    #+#             */
-/*   Updated: 2023/09/01 09:37:45 by arnaud           ###   ########.fr       */
+/*   Updated: 2023/09/01 09:39:51 by arnaud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char *s_ptr;
 	unsigned char *c_ptr;
-	int i;
+	size_t i;
 
 	i = -1;
 	s_ptr = (unsigned char *)s;
-	c_ptr = (unsigned char *)c;
+	c_ptr = (unsigned char)c;
 
-	while (++i < (int)n)
+	while (++i < n)
 	{
 		if (s_ptr[i] == c_ptr)
 			return (s + i);
