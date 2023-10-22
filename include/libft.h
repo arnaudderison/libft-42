ba8842bb085e7456d31a916640e961b0adaf736c
arnaudderison <arnaud.derison@hotmail.com>
2023-10-22 16:59:41 +0200

    add prototypes

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: arnaud <arnaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 22:41:28 by arnaud            #+#    #+#             */
-/*   Updated: 2023/10/20 16:53:06 by arnaud           ###   ########.fr       */
+/*   Updated: 2023/10/22 16:56:43 by arnaud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,5 +58,7 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(const char *s);
 char	**ft_strsplit(const char *s, char c);
+void	ft_putchar(char c);
+void	ft_putstr(const char *str);
 
 #endif
